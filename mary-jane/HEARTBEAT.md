14043a8aa9d8b86178c3dd1f268657f525e19e7a
tylergarlick <tyler@garlick.com>
2026-05-26 17:18:53 +0000

    Remove erroneously moved mary-jane repository files

--- mary-jane/HEARTBEAT.md
+++
@@ -1,7 +0,0 @@
-# HEARTBEAT.md
-
-# Periodic checks go below:
-
----
-# Ollama startup:
-# Run: ./start_ollama.sh (from workspace folder)
--- mary-jane/scripts/post-completion-pipeline.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-// Placeholder post-completion pipeline
-console.log('post-completion-pipeline placeholder executed');
